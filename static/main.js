d9cc6b98ee5ee3d66020d4ce70a0c4fba5b28ef5
EngiBeerHub <engibeer0422@gmail.com>
2025-03-22 19:07:34 +0900

    [10-53]盤面の表示機能を実装し、ターンを登録するAPIを追加

--- static/main.js
+++ static/main.js
@@ -2,27 +2,25 @@ const EMPTY = 0;
 const DARK = 1;
 const LIGHT = 2;
 
-const INITIAL_BOARD = [
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-];
-
 const boardElement = document.getElementById("board");
 
+/**
+ * 盤面を表示する
+ */
 async function showBoard() {
-  // Clear the board
+  const turnCount = 0;
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+  const responseBody = await response.json();
+  const board = responseBody.board;
+  const nextDisc = responseBody.nextDisc;
+
+  // 盤面を初期化
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
   }
 
-  INITIAL_BOARD.forEach((line) => {
-    line.forEach((square) => {
+  board.forEach((line, y) => {
+    line.forEach((square, x) => {
       const squareElement = document.createElement("div");
       squareElement.className = "square";
 
@@ -31,6 +29,11 @@ async function showBoard() {
         const color = square === DARK ? "dark" : "light";
         stoneElement.className = `stone ${color}`;
         squareElement.appendChild(stoneElement);
+      } else {
+        squareElement.addEventListener("click", async () => {
+          const nextTurnCount = turnCount + 1;
+          await registerTurn(nextTurnCount, nextDisc, x, y);
+        });
       }
 
       boardElement.appendChild(squareElement);
@@ -44,6 +47,32 @@ async function showBoard() {
 async function registerGame() {
   await fetch("/api/games", {
     method: "POST",
+  });
+}
+
+/**
+ * ターンを登録する
+ * @param {*} turnCount
+ * @param {*} disc
+ * @param {*} x
+ * @param {*} y
+ */
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y,
+    },
+  };
+
+  await fetch("/api/games/latest/turns", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
   });
 }
 
