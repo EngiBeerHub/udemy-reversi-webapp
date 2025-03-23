2c559a6e0ae4b20135a37351d7f63d33ab71daab
EngiBeerHub <engibeer0422@gmail.com>
2025-03-23 21:45:56 +0900

    [11-61]サービスクラスの作成

---
+++ src/application/gameService.ts
@@ -0,0 +1,37 @@
+import { DARK, INITIAL_BOARD } from "../application/constants";
+import { connectMySQL } from "../dataaccess/connection";
+import { GameGateway } from "../dataaccess/gameGateway";
+import { SquareGateway } from "../dataaccess/squareGateway";
+import { TrunGateway } from "../dataaccess/turnGateway";
+
+// Table Data Gatewayの宣言
+const gameGateway = new GameGateway();
+const turnGateway = new TrunGateway();
+const squareGateway = new SquareGateway();
+
+export class GameService {
+  async startNewGame() {
+    const now = new Date();
+
+    const conn = await connectMySQL();
+    try {
+      await conn.beginTransaction();
+
+      const gameRecord = await gameGateway.insert(conn, now);
+
+      const turnRecord = await turnGateway.insert(
+        conn,
+        gameRecord.id,
+        0,
+        DARK,
+        now
+      );
+
+      await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD);
+
+      await conn.commit();
+    } finally {
+      await conn.end();
+    }
+  }
+}
--- src/presentation/gameRouter.ts
+++ src/presentation/gameRouter.ts
@@ -5,40 +5,16 @@ import { MoveGateway } from "../dataaccess/moveGateway";
 import { SquareGateway } from "../dataaccess/squareGateway";
 import { connectMySQL } from "../dataaccess/connection";
 import { DARK, INITIAL_BOARD } from "../application/constants";
+import { GameService } from "../application/gameService";
 
 export const gameRouter = express.Router();
 
-// Table Data Gatewayの宣言
-const gameGateway = new GameGateway();
-const turnGateway = new TrunGateway();
-const squareGateway = new SquareGateway();
+const gameService = new GameService();
 
 /**
  * 対戦を開始するAPI
  */
 gameRouter.post("/api/games", async (req, res) => {
-  const now = new Date();
-
-  const conn = await connectMySQL();
-  try {
-    await conn.beginTransaction();
-
-    const gameRecord = await gameGateway.insert(conn, now);
-
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      0,
-      DARK,
-      now
-    );
-
-    await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD);
-
-    await conn.commit();
-  } finally {
-    await conn.end();
-  }
-
+  await gameService.startNewGame();
   res.status(201).end();
 });
