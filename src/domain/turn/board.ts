01ddab95b78a17513adbf3be5e063d250396e76a
EngiBeerHub <engibeer0422@gmail.com>
2025-03-29 18:09:12 +0900

    [13-77]ひっくり返せる点をリストアップする処理の実装

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,29 +1,107 @@
-import { Disc } from "./disc";
-import { Move } from "./move";
+import {Disc, isOppositeDisc} from "./disc";
+import {Move} from "./move";
+import {Point} from "./point";
 
 export class Board {
-  constructor(private _discs: Disc[][]) {}
+    private _walledDiscs: Disc[][];
 
-  place(move: Move): Board {
-    // TODO 盤面に置けるかチェック
-    // 盤面をコピー
-    const newDiscs = this._discs.map((line) => {
-      return line.map((disc) => {
-        return disc;
-      });
-    });
+    constructor(private _discs: Disc[][]) {
+        this._walledDiscs = this.wallDiscs();
+    }
 
-    // 石を置く
-    newDiscs[move.point.y][move.point.x] = move.disc;
+    place(move: Move): Board {
+        // TODO 盤面に置けるかチェック
+        // 空のマス目ではない場合、置くことはできない
+        if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
+            throw new Error('Selected point is not empty');
+        }
 
-    // TODO ひっくり返す
+        // ひっくり返せる点をリストアップ
+        const flipPoints = this.listFlipPoints(move);
 
-    return new Board(newDiscs);
-  }
+        // ひっくり返せる点がない場合、置くことはできない
+        if (flipPoints.length === 0) {
+            throw new Error("Flip points is empty");
+        }
 
-  get discs() {
-    return this._discs;
-  }
+        // 盤面をコピー
+        const newDiscs = this._discs.map((line) => {
+            return line.map((disc) => {
+                return disc;
+            });
+        });
+
+        // 石を置く
+        newDiscs[move.point.y][move.point.x] = move.disc;
+
+        // TODO ひっくり返す
+
+        return new Board(newDiscs);
+    }
+
+    private listFlipPoints(move: Move): Point[] {
+        const flipPoints: Point[] = [];
+
+        const walledX = move.point.x + 1;
+        const walledY = move.point.y + 1;
+
+        const checkFlipPoints = (xMove: number, yMove: number) => {
+            const flipCandidate: Point[] = [];
+
+            // 一つ動いた位置から開始
+            let cursorX = walledX + xMove;
+            let cursorY = walledY + yMove;
+
+            // 手と逆の色の石がある間、一つずつ見ていく
+            while (isOppositeDisc(move.disc, this._walledDiscs[cursorY][cursorX])) {
+                // 番兵を考慮して-1する
+                flipCandidate.push(new Point(cursorX - 1, cursorY - 1));
+                cursorX += xMove;
+                cursorY += yMove;
+                // 次の手が同じ色の石なら、ひっくり返す石が確定
+                if (move.disc === this._walledDiscs[cursorY][cursorX]) {
+                    flipPoints.push(...flipCandidate);
+                    break;
+                }
+            }
+        };
+
+        // 上
+        checkFlipPoints(0, -1);
+        // 左上
+        checkFlipPoints(-1, -1);
+        // 左
+        checkFlipPoints(-1, 0);
+        // 左下
+        checkFlipPoints(-1, 1);
+        // 下
+        checkFlipPoints(0, 1);
+        // 右下
+        checkFlipPoints(1, 1);
+        // 右
+        checkFlipPoints(1, 0);
+        // 右上
+        checkFlipPoints(1, -1);
+
+        return flipPoints;
+    }
+
+    private wallDiscs(): Disc[][] {
+        const walled: Disc[][] = [];
+        const topAndBottomWall = Array(this._discs[0].length + 2).fill(Disc.Wall);
+        walled.push(topAndBottomWall);
+        this._discs.forEach(line => {
+            const walledLine = [Disc.Wall, ...line, Disc.Wall];
+            walled.push(walledLine);
+        });
+        walled.push(topAndBottomWall);
+
+        return walled;
+    }
+
+    get discs() {
+        return this._discs;
+    }
 }
 
 const E = Disc.Empty;
@@ -31,14 +109,14 @@ const D = Disc.Dark;
 const L = Disc.Light;
 
 const INITIAL_BOARD = [
-  [E, E, E, E, E, E, E, E],
-  [E, E, E, E, E, E, E, E],
-  [E, E, E, E, E, E, E, E],
-  [E, E, E, D, L, E, E, E],
-  [E, E, E, L, D, E, E, E],
-  [E, E, E, E, E, E, E, E],
-  [E, E, E, E, E, E, E, E],
-  [E, E, E, E, E, E, E, E],
+    [E, E, E, E, E, E, E, E],
+    [E, E, E, E, E, E, E, E],
+    [E, E, E, E, E, E, E, E],
+    [E, E, E, D, L, E, E, E],
+    [E, E, E, L, D, E, E, E],
+    [E, E, E, E, E, E, E, E],
+    [E, E, E, E, E, E, E, E],
+    [E, E, E, E, E, E, E, E]
 ];
 
 export const initialBoard = new Board(INITIAL_BOARD);
