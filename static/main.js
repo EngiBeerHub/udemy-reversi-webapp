20ecd6938c8b9fcd66225cdd82db8bdbb6a8ecbe
EngiBeerHub <engibeer0422@gmail.com>
2025-04-13 16:23:38 +0900

    [13-81]フロントエンドの改善

--- static/main.js
+++ static/main.js
@@ -3,51 +3,64 @@ const DARK = 1;
 const LIGHT = 2;
 
 const boardElement = document.getElementById("board");
+const nextDiscMessageElement = document.getElementById('next-disc-message');
 
 /**
  * 盤面を表示する
  */
 async function showBoard(turnCount) {
-  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
-  const responseBody = await response.json();
-  const board = responseBody.board;
-  const nextDisc = responseBody.nextDisc;
+    const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+    const responseBody = await response.json();
+    const board = responseBody.board;
+    const nextDisc = responseBody.nextDisc;
+    showNextDiscMessage(nextDisc);
 
-  // 盤面を初期化
-  while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild);
-  }
+    // 盤面を初期化
+    while (boardElement.firstChild) {
+        boardElement.removeChild(boardElement.firstChild);
+    }
 
-  board.forEach((line, y) => {
-    line.forEach((square, x) => {
-      const squareElement = document.createElement("div");
-      squareElement.className = "square";
+    board.forEach((line, y) => {
+        line.forEach((square, x) => {
+            const squareElement = document.createElement("div");
+            squareElement.className = "square";
 
-      if (square !== EMPTY) {
-        const stoneElement = document.createElement("div");
-        const color = square === DARK ? "dark" : "light";
-        stoneElement.className = `stone ${color}`;
-        squareElement.appendChild(stoneElement);
-      } else {
-        squareElement.addEventListener("click", async () => {
-          const nextTurnCount = turnCount + 1;
-          await registerTurn(nextTurnCount, nextDisc, x, y);
-          await showBoard(nextTurnCount);
+            if (square !== EMPTY) {
+                const stoneElement = document.createElement("div");
+                const color = square === DARK ? "dark" : "light";
+                stoneElement.className = `stone ${color}`;
+                squareElement.appendChild(stoneElement);
+            } else {
+                squareElement.addEventListener("click", async () => {
+                    const nextTurnCount = turnCount + 1;
+                    const registerTurnResponse = await registerTurn(nextTurnCount, nextDisc, x, y);
+                    if (registerTurnResponse.ok) {
+                        await showBoard(nextTurnCount);
+                    }
+                });
+            }
+
+            boardElement.appendChild(squareElement);
         });
-      }
-
-      boardElement.appendChild(squareElement);
     });
-  });
+}
+
+function showNextDiscMessage(nextDisc) {
+    if (nextDisc) {
+        const color = nextDisc === DARK ? '黒' : '白';
+        nextDiscMessageElement.innerText = `次は${color}の番です`;
+    } else {
+        nextDiscMessageElement.innerText = '';
+    }
 }
 
 /**
  * 対戦を開始する
  */
 async function registerGame() {
-  await fetch("/api/games", {
-    method: "POST",
-  });
+    await fetch("/api/games", {
+        method: "POST"
+    });
 }
 
 /**
@@ -58,27 +71,27 @@ async function registerGame() {
  * @param {*} y
  */
 async function registerTurn(turnCount, disc, x, y) {
-  const requestBody = {
-    turnCount,
-    move: {
-      disc,
-      x,
-      y,
-    },
-  };
+    const requestBody = {
+        turnCount,
+        move: {
+            disc,
+            x,
+            y
+        }
+    };
 
-  await fetch("/api/games/latest/turns", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestBody),
-  });
+    return await fetch("/api/games/latest/turns", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+    });
 }
 
 async function main() {
-  await registerGame();
-  await showBoard(0);
+    await registerGame();
+    await showBoard(0);
 }
 
 main();
