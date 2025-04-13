7df51a491f08abdc7d618d7af49c227ba63cb835
EngiBeerHub <engibeer0422@gmail.com>
2025-04-13 15:53:32 +0900

    [13-79]ドメイン層のエラーハンドリングの実装

--- src/domain/model/turn/board.ts
+++ src/domain/model/turn/board.ts
@@ -1,6 +1,7 @@
 import {Disc, isOppositeDisc} from "./disc";
 import {Move} from "./move";
 import {Point} from "./point";
+import {DomainError} from "../../error/domainError";
 
 export class Board {
     private _walledDiscs: Disc[][];
@@ -13,7 +14,7 @@ export class Board {
         // TODO 盤面に置けるかチェック
         // 空のマス目ではない場合、置くことはできない
         if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
-            throw new Error('Selected point is not empty');
+            throw new DomainError('SelectedPointIsNotEmpty', 'Selected point is not empty');
         }
 
         // ひっくり返せる点をリストアップ
@@ -21,7 +22,7 @@ export class Board {
 
         // ひっくり返せる点がない場合、置くことはできない
         if (flipPoints.length === 0) {
-            throw new Error("Flip points is empty");
+            throw new DomainError('FlipPointsIsEmpty', "Flip points is empty");
         }
 
         // 盤面をコピー
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-import { gameRouter } from "./presentation/gameRouter";
-import { turnRouter } from "./presentation/turnRouter";
+import {gameRouter} from "./presentation/gameRouter";
+import {turnRouter} from "./presentation/turnRouter";
+import {DomainError} from "./domain/error/domainError";
 
 const PORT = 3000;
 
@@ -10,7 +11,7 @@ const app = express();
 
 // アプリケーションの設定
 app.use(morgan("dev"));
-app.use(express.static("static", { extensions: ["html"] }));
+app.use(express.static("static", {extensions: ["html"]}));
 app.use(express.json());
 
 app.use(gameRouter);
@@ -19,17 +20,25 @@ app.use(turnRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Reversi application started: http://localhost:${PORT}`);
+    console.log(`Reversi application started: http://localhost:${PORT}`);
 });
 
 function errorHandler(
-  err: any,
-  _req: express.Request,
-  res: express.Response,
-  _next: express.NextFunction
+    err: any,
+    _req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction
 ) {
-  console.error("Unexpected error occurred", err);
-  res.status(500).send({
-    message: "Unexpected error occurred",
-  });
+    if (err instanceof DomainError) {
+        res.status(400).json({
+            type: err.type,
+            message: err.message
+        });
+        return;
+    }
+
+    console.error("Unexpected error occurred", err);
+    res.status(500).send({
+        message: "Unexpected error occurred"
+    });
 }
